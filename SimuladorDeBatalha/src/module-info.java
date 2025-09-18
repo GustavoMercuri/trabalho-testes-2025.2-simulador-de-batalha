6eb1b3c9a8636d530fc39cdbc6aee1ea37630061
Rian Benedito <rian.bene14@gmail.com>
2025-09-18 09:37:49 -0300

    Implementa Criatura, Item e dependencias
    Criatura e Item v1
    JUnit5 e Mockito no projeto

---
+++ SimuladorDeBatalha/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SimuladorDeBatalha {
+	requires org.junit.jupiter.api;
+	requires org.mockito.junit.jupiter;
+	requires org.mockito;
+}
---
+++ SimuladorDeBatalha/src/test/CriaturaTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+
+import model.Criatura;
+
+import org.junit.jupiter.api.Test;
+
+class CriaturaTest {
+	@Mock
+	private Criatura criatura;
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}

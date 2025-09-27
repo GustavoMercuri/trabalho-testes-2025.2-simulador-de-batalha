ad8dc59ba518c644aa88ebdacdab45e59a1f7fef
Rian Benedito <rian.bene14@gmail.com>
2025-09-27 15:05:58 -0300

    Update GerenciadorDeBatalha.java

--- SimuladorDeBatalha/src/service/GerenciadorDeBatalha.java
+++ SimuladorDeBatalha/src/service/GerenciadorDeBatalha.java
@@ -3,20 +3,31 @@ package service;
 import model.entidades.Criatura;
 
 public class GerenciadorDeBatalha {
-    private Criatura criaturaA;
-    private Criatura criaturaB;
-    private int turno;
+    private Criatura criatura1;
+    private Criatura criatura2;
+    private int contTurnos;
 
-    public GerenciadorDeBatalha(Criatura criaturaA, Criatura criaturaB) {
-        this.criaturaA = criaturaA;
-        this.criaturaB = criaturaB;
-        this.turno = 1;
+    public GerenciadorDeBatalha(Criatura criatura1, Criatura criatura2) {
+        this.criatura1 = criatura1;
+        this.criatura2 = criatura2;
+        this.contTurnos = 1;
     }
 
     public void batalha(){
-        while(criaturaA.estaViva() && criaturaB.estaViva()){
-            
-            turno++;
+        int turno = (criatura1.getVelocidade() > criatura2.getVelocidade()) ? 0 : 1;
+
+        while(criatura1.estaViva() && criatura2.estaViva()){
+            if (turno == 0){
+                //Ataque da criatura1
+            } else {
+                //Ataque da criatura2
+            }
+            contTurnos++;
+        }
+        if (criatura1.estaViva() && !criatura2.estaViva()){
+            System.out.println("Vencedor:" + criatura1.getNome());
+        } else{
+            System.out.println("Vencedor:" + criatura2.getNome());
         }
     }
 }
